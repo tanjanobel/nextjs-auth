879f78762ebbf6db18d0afe6e807a834e2ba5e3a
Tanja Nobel <tanja@nobel.swiss>
2022-07-21 11:03:02 +0200

    Improve signup with unique email addresses

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -28,6 +28,14 @@ const handler = async (req, res) => {
 
     const db = client.db();
 
+    const existingUser = await db.collection('users').findOne({ email: email });
+
+    if (existingUser) {
+        res.status(422).json({message: 'User exists already!'});
+        client.close();
+        return;
+    }
+
     const hashedPassword = await hashPassword(password);
 
     const result = await db.collection('users').insertOne({
@@ -36,6 +44,7 @@ const handler = async (req, res) => {
     });
 
     res.status(201).json({ message: 'Created user!' });
+    client.close();
 }
 
 export default handler;
