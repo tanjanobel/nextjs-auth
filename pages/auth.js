a26fd076977894d60d9c617e1d3baa3f64266454
Tanja Nobel <tanja@nobel.swiss>
2022-07-24 14:13:10 +0200

    Protect the auth page

--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,28 @@
+import { useRouter } from "next/router";
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+
 import AuthForm from '../components/auth/auth-form';
 
 function AuthPage() {
-  return <AuthForm />;
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        getSession().then((session) => {
+            if (session) {
+                router.replace('/');
+            } else {
+                setIsLoading(false);
+            }
+        });
+    }, [router]);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    return <AuthForm />;
 }
 
 export default AuthPage;
